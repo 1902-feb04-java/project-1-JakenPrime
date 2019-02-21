273423b7899569180dbfa6a377028e02fbc39f65
Jacob Dean <JacobDean117@gmail.com>
2019-02-21 16:39:15 -0600

    added basic scanner input

--- ERP/src/main/java/com/revature/App.java
+++ ERP/src/main/java/com/revature/App.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Please Enter Info");
+        Scanner scanner = new Scanner(System.in);
+        // scanner.useDelimiter(" ");
+
+        String id = scanner.next();
+        String amount = scanner.next();
+        String manager = scanner.next();
+
+        System.out.println("Employee ID: " + id);
+        System.out.println("Reimbursement amount: "+ amount);
+        System.out.println("Manager ID: " + manager);
+        
+        scanner.close();
     }
 }
